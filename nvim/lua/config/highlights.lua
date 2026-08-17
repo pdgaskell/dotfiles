a7fac92e05f379bef2986c71001e7bccb4c2a829
Phil Gaskell <pdgaskell@gmail.com>
2026-08-17 21:49:08 +1200

    Improves highlighting of trailing spaces.

--- nvim/lua/config/highlights.lua
+++ nvim/lua/config/highlights.lua
@@ -1,21 +1,11 @@
-local group = vim.api.nvim_create_augroup("extra-whitespace", { clear = true })
-
-local function highlight_extra_whitespace()
-    vim.cmd([[highlight ExtraWhitespace ctermbg=lightred guibg=lightred]])
-
-    if vim.w.extra_whitespace_match_id then
-        vim.fn.matchdelete(vim.w.extra_whitespace_match_id)
-    end
-
-    vim.w.extra_whitespace_match_id = vim.fn.matchadd(
-        "ExtraWhitespace",
-        [[\(\t\+\|\s\+$\)]]
-    )
-end
-
-highlight_extra_whitespace()
-
-vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter", "ColorScheme" }, {
-    group = group,
-    callback = highlight_extra_whitespace,
+ -- Highlights trailing whitespace.
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+    pattern = "*",
+    callback = function()
+        vim.fn.matchadd('Special', [[\s\+$]])
+    end,
 })
+
+-- Replaces trailing spaces with dots
+vim.opt.list = true
+vim.opt.listchars:append({ trail = '·' })
